2e73bf601e7176fb8d6267da6240537119e5a787
Arthur Costa <arthurcosta007110@gmail.com>
2024-02-14 00:39:46 -0300

    implementando tokenizacao e identificacao de duplicatas utilizando NLP

--- preprocess.py
+++ preprocess.py
@@ -1,50 +1,79 @@
-#import kmeans
-from sklearn.cluster import KMeans
-import numpy as np
-import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
+import pandas as pd
+import numpy as np
 
-
-
-
-# reading the database from base folder base/Buy.csv
+# Carregar os dados dos arquivos CSV
 buy = pd.read_csv('base/Buy.csv')
-abt = pd.read_csv('base/Abt.csv', encoding_errors='ignore')
+abt = pd.read_csv('base/Abt.csv')
 
-# remove columns that are not needed
-buy.drop(['id', 'manufacturer', 'price'], axis=1, inplace=True)
-abt.drop(['id', 'price'], axis=1, inplace=True)
+# Preencher valores nulos na coluna de preço com a média
+buy['price'] = pd.to_numeric(buy['price'], errors='coerce')
+buy['price'].fillna(buy['price'].mean(), inplace=True)
+abt['price'] = pd.to_numeric(abt['price'], errors='coerce')
+abt['price'].fillna(abt['price'].mean(), inplace=True)
 
-# convert the all to lower case
-buy = buy.apply(lambda x: x.astype(str).str.lower())
-abt = abt.apply(lambda x: x.astype(str).str.lower())
+# Substituir valores nulos nas colunas de texto por uma string vazia
+buy['name'].fillna('', inplace=True)
+buy['description'].fillna('', inplace=True)
+abt['name'].fillna('', inplace=True)
+abt['description'].fillna('', inplace=True)
 
-# taking just 500 items to test using train_test_split
-trainBuy, testBuy = train_test_split(buy['name'], test_size=0.7, random_state=1)
-trainAbt, testAbt = train_test_split(abt['name'], test_size=0.7, random_state=1)
+# Tokenização e Vetorização para Buy.csv
+tfidf_vectorizer_buy = TfidfVectorizer()
+tfidf_matrix_buy = tfidf_vectorizer_buy.fit_transform(
+    buy['name'] + ' ' + buy['description'])
 
-# padronize the size of the data
-trainBuy = trainBuy[:1]
-trainAbt = trainAbt[:1]
+# Cálculo da Similaridade de Cosseno para Buy.csv
+cosine_sim_buy = cosine_similarity(tfidf_matrix_buy, tfidf_matrix_buy)
 
-# segment the data, first vectorize the data
-# https://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html#sklearn.feature_extraction.text.CountVectorizer
-vectorizer = CountVectorizer()
-buyVectors = vectorizer.fit_transform(trainBuy)
-abtVectors = vectorizer.fit_transform(trainAbt)
-print(str(trainBuy))
-print(str(trainAbt))
-# cosine similarity of the common items
-# https://scikit-learn.org/stable/modules/generated/sklearn.metrics.pairwise.cosine_similarity.html
-cosine_sim = cosine_similarity(buyVectors, abtVectors)
-print(cosine_sim)
-for i in range(len(cosine_sim)):
-    if(cosine_sim[i][i] > 0.5):
-        print(buy[i], abt[i])
-        break
+# Definição de um limiar de similaridade
+threshold = 0.8
 
-#print(buy.head())
-#print(abt.head())
+# Identificação de Duplicatas em Buy.csv
+duplicates_buy = []
+for i in range(len(cosine_sim_buy)):
+    for j in range(i+1, len(cosine_sim_buy[i])):
+        if cosine_sim_buy[i][j] > threshold:
+            duplicates_buy.append(
+                (buy.iloc[i]['name'], buy.iloc[i]['description'], buy.iloc[j]['name'], buy.iloc[j]['description']))
 
+# Tokenização e Vetorização para Abt.csv
+tfidf_vectorizer_abt = TfidfVectorizer()
+tfidf_matrix_abt = tfidf_vectorizer_abt.fit_transform(
+    abt['name'] + ' ' + abt['description'])
+
+# Cálculo da Similaridade de Cosseno para Abt.csv
+cosine_sim_abt = cosine_similarity(tfidf_matrix_abt, tfidf_matrix_abt)
+
+# Identificação de Duplicatas em Abt.csv
+duplicates_abt = []
+for i in range(len(cosine_sim_abt)):
+    for j in range(i+1, len(cosine_sim_abt[i])):
+        if cosine_sim_abt[i][j] > threshold:
+            duplicates_abt.append(
+                (abt.iloc[i]['name'], abt.iloc[i]['description'], abt.iloc[j]['name'], abt.iloc[j]['description']))
+
+# Função para exibir um par de duplicatas de forma mais legível
+
+
+def print_pair(pair):
+    print("Duplicata 1:")
+    print("Nome: " + pair[0])
+    print("Descrição: " + pair[1])
+    print("\n")
+    print("Duplicata 2:")
+    print("Nome: " + pair[2])
+    print("Descrição: " + pair[3])
+    print("\n")
+
+
+# Visualização das Duplicatas em Buy.csv
+print("Duplicatas em Buy.csv Identificadas:")
+for pair in duplicates_buy:
+    print_pair(pair)
+
+# Visualização das Duplicatas em Abt.csv
+print("\nDuplicatas em Abt.csv Identificadas:")
+for pair in duplicates_abt:
+    print_pair(pair)
