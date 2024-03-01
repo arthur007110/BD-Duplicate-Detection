b4667762e8b94fb8135dbc47862866dc1a4b6199
IvandoNascimento <nascimentodoivan@gmail.com>
2024-02-29 23:12:55 -0300

    limpando, c

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,7 @@
 import csv
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
-# Função para tokenizar e calcular a similaridade entre os tokens
+import time
 
 
 def calculate_similarity(items_a, items_b):
@@ -12,28 +11,25 @@ def calculate_similarity(items_a, items_b):
     similarity_matrix = cosine_similarity(tfidf_matrix_a, tfidf_matrix_b)
     return similarity_matrix
 
-# Leitura dos arquivos CSV
-
 
 def read_csv(file_path, important_fields):
     data = {}
-    with open(file_path, newline='', encoding='utf-8') as csvfile:
+    with open(file_path, newline='', encoding='utf-8', errors='ignore') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            item_id = row["id"]
+            item_id = row["id"] # n
             item_data = " ".join([row[field] for field in important_fields])
             data[item_id] = item_data
     return data
 
+start = time.time()
 
-# Leitura dos arquivos Abt.csv e Buy.csv
 abt_data = read_csv('base/Abt.csv', ['name', 'description'])
 buy_data = read_csv('base/Buy.csv', ['name', 'description'])
 
-# Tokenização e cálculo da similaridade
 abt_items = list(abt_data.values())
-buy_items = list(buy_data.values())
-similarity_matrix = calculate_similarity(abt_items, buy_items)
+buy_items = list(buy_data.values()) 
+similarity_matrix = calculate_similarity(abt_items, buy_items) # n^2
 
 # Definição de um limiar de similaridade
 # 0.8 - Precisão 100% 1.0, baixissima revocação 0.02, F-measure 0.04
@@ -42,31 +38,27 @@ similarity_matrix = calculate_similarity(abt_items, buy_items)
 # 0.5 - Precisão 0.60, revocação 0.56, F-measure 0.58 - Melhor resultado
 # 0.55 - Precisão 0.71, revocação 0.44, F-measure 0.54
 # 0.4 - Precisão 0.35, revocação 0.76, F-measure 0.48
-threshold = 0.55
+threshold = 0.5
 
-# Identificação das duplicatas potenciais
 duplicates = []
 for i, abt_item in enumerate(abt_items):
-    for j, buy_item in enumerate(buy_items):
-        if similarity_matrix[i][j] >= threshold:
+    for j, buy_item in enumerate(buy_items): # n^2 for de for
+        if similarity_matrix[i][j] >= threshold: 
             duplicates.append(
                 (list(abt_data.keys())[i], list(buy_data.keys())[j]))
 
-# Escrita dos pares de IDs dos itens duplicados em um arquivo CSV
 with open('result.csv', mode='w', newline='', encoding='utf-8') as csvfile:
     writer = csv.writer(csvfile)
     writer.writerow(['abt_id', 'buy_id'])
     for abt_id, buy_id in duplicates:
         writer.writerow([abt_id, buy_id])
 
-# Leitura do arquivo de mapeamento para validação
 validation_data = []
 with open('base/abt_buy_perfectMapping.csv', newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         validation_data.append((row["idAbt"], row["idBuy"]))
 
-# Validação das duplicatas
 true_positives = 0
 for abt_id, buy_id in duplicates:
     if (abt_id, buy_id) in validation_data:
@@ -75,7 +67,8 @@ for abt_id, buy_id in duplicates:
 precision = true_positives / len(duplicates) if len(duplicates) > 0 else 0
 recall = true_positives / \
     len(validation_data) if len(validation_data) > 0 else 0
-f_measure = 2 * (precision * recall) / (precision +
-                                        recall) if (precision + recall) > 0 else 0
-
+f_measure = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+end = time.time()
 print(f"Precision: {precision}, Recall: {recall}, F-Measure: {f_measure}")
+
+print(f"Tempo de execução: {end - start} segundos")
